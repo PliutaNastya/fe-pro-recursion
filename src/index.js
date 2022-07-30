05225c058e70d60f209668e81fbb6a97a3e7a51d
Anastasiia <koicheva1996@gmail.com>
2022-07-30 14:41:11 +0300

    added homework(recursion)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@
  * true, если есть различие, false. То есть проверяет каждое свойство, вне зависимости от вложенности,
  * делаем через рекурсию(а других вариантов и нет)
  */
-export const deepEqual = (obj, anotherObject) => {};
+export const deepEqual = (obj, anotherObject) => {
+  function deepValue(object) {
+    return Object.values(object).reduce((accum, value) => {
+      if (typeof value === 'object') {
+        return [...accum, ...deepValue(value)].toString();
+      }
+      return [...accum, value].toString();
+    }, [].toString());
+  }
+
+  return deepValue(obj) === deepValue(anotherObject);
+};
 
 /**
  * Принимает объект, возвращает его глубокую копию, то есть ни одно свойство
@@ -12,10 +23,29 @@ export const deepEqual = (obj, anotherObject) => {};
  * то тогда в рекурсию. С объектом также. Поскольку массив при typeof возвращает object, чтобы
  * их различить берем метод Array.isArray и он на массивах вернет тру
  */
-export const deepCopy = (obj) => {};
+export const deepCopy = (obj) => {
+  if (Array.isArray(obj)) {
+    return obj.map(deepCopy);
+  }
+  if (typeof obj !== 'object') {
+    return obj;
+  }
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, deepCopy(value)])
+  );
+};
 
 /**
  * Мы передаем объект, и должны вернуть массив уникальных названий свойств
  * То есть если у нас объект { name: { bohdan: { name: 'test' } } } вернет ['name', 'bohdan']
  */
-export const getAllObjectKeys = (obj) => {};
+export const getAllObjectKeys = (obj) => {
+  return Object.entries(obj).reduce((accum, [key, value]) => {
+    if (typeof value === 'object') {
+      accum.push(...[key, ...getAllObjectKeys(value)]);
+    } else {
+      accum.push(key);
+    }
+    return [...new Set(accum)];
+  }, []);
+};
